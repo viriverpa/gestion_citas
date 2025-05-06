3c46009d5c3536b843911d7847c8e2f81a6ce7ca
Victor <viriverpa@hotmail.com>
2025-05-06 00:11:22 -0500

    🔧 coloque PREPEND_WWW FALSE para evitar bucle de redirección

--- gestion_citas/settings.py
+++ gestion_citas/settings.py
@@ -35,7 +35,7 @@ if not DEBUG:
     SECURE_HSTS_SECONDS = 31536000
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
-
+    PREPEND = False
 # ------------------------------
 # APLICACIONES
 # ------------------------------
